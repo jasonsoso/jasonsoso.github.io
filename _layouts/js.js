e5948fa398875c4cf9bb75723f0810411a5daa94
jasonsoso <648636045@qq.com>
2015-01-02 12:12:35 +0800

    update css and js

---
+++ _layouts/js.js
@@ -0,0 +1,10 @@
+/* jQuery Version 1.11.0 */
+{% include js/jquery-1.11.0.js %}
+/* Bootstrap Core JavaScript */
+{% include js/bootstrap.min.js %}
+/* Plugin JavaScript*/
+{% include js/jquery.easing.min.js %}
+{% include js/classie.js %}
+{% include js/cbpAnimatedHeader.js %}
+/* Custom Theme JavaScript*/
+{% include js/agency.js %}
